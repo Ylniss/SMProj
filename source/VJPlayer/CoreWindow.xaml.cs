c4c50bec2170ee4fe5cae41a4cde434b9ade73db
Rogerotti <Rogerottii@gmail.com>
2016-03-03 13:04:05 +0100

    Event z wrzucaniem plików

---
+++ source/VJPlayer/CoreWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace VJPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CoreWindow : Window
+    {
+        public CoreWindow()
+        {
+            InitializeComponent();
+            Drop += CoreWindow_Drop;
+           
+        }
+
+        private void CoreWindow_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                var d = files[0];
+                OpenFileDialog s = new OpenFileDialog();
+                s.
+              
+            }
+
+        }
+
+    }
+}
